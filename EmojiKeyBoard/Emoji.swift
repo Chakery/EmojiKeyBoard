b5f53dc2cb2460af407e5e0aedeebdf3fae25224
Chakery <chakery@chakery.local>
2016-03-07 13:54:17 +0800

    add

--- EmojiKeyBoard/Emoji.swift
+++ EmojiKeyBoard/Emoji.swift
@@ -12,11 +12,11 @@ import UIKit
 struct EmojiModel {
 	var id: String? // 表情对应的包ID
 	var chs: String? // 表情发送时的对应的文本(简体中文)
-	var cht: String? // 表情发送时的对应的文本(繁体中文)
-	var gif: String? // Gif表情名称
+//	var cht: String? // 表情发送时的对应的文本(繁体中文)
+//	var gif: String? // Gif表情名称
 	var png: String? // Png表情名称
 	var code: String? // 表情十六进制编码
-	var gifImage: UIImage? // gif图片路径
+//	var gifImage: UIImage? // gif图片路径
 	var pngImage: UIImage? // png图片路径
 	var emoji: String? // 表情
 	var deleteBtn: Bool // false: 表情 | true: 删除按钮
@@ -24,8 +24,8 @@ struct EmojiModel {
 	init(id: String?, dic: [String : AnyObject]?, deleteBtn: Bool = false) {
 		self.id = id
 		self.chs = dic?["chs"] as? String
-		self.cht = dic?["cht"] as? String
-		self.gif = dic?["gif"] as? String
+//		self.cht = dic?["cht"] as? String
+//		self.gif = dic?["gif"] as? String
 		self.png = dic?["png"] as? String
 		self.code = dic?["code"] as? String
 		self.deleteBtn = deleteBtn
@@ -33,9 +33,9 @@ struct EmojiModel {
 		if let png = self.png {
 			self.pngImage = UIImage(named: "EmojiKeyBoard.bundle/\(id!)/\(png)")
 		}
-		if let gif = self.gif {
-			self.gifImage = UIImage(named: "EmojiKeyBoard.bundle/\(id!)/\(gif)")
-		}
+//		if let gif = self.gif {
+//			self.gifImage = UIImage(named: "EmojiKeyBoard.bundle/\(id!)/\(gif)")
+//		}
 		if deleteBtn {
 			self.pngImage = UIImage(named: "EmojiKeyBoard.bundle/delete.png")
 		}
